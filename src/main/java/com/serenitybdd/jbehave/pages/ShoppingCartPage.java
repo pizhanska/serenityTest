689e65134a11aacbca7931c781184b636c20f89e
pizhanska <pizhanska29@gmail.com>
2018-02-20 23:59:09 +0200

    upd shopping cart page

--- src/main/java/com/serenitybdd/jbehave/pages/ShoppingCartPage.java
+++ src/main/java/com/serenitybdd/jbehave/pages/ShoppingCartPage.java
@@ -3,6 +3,7 @@ package com.serenitybdd.jbehave.pages;
 import net.serenitybdd.core.pages.WebElementFacade;
 import net.thucydides.core.pages.PageObject;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 import java.util.ArrayList;
@@ -10,13 +11,15 @@ import java.util.List;
 
 public class ShoppingCartPage extends PageObject {
     @FindBy (xpath = "//ul[@class='list-group']/li")
-    private List<WebElementFacade> itemsInCart;
+    private WebElementFacade itemInCart;
 
-    public List<String> getItemsInCart() {
-        List<String> items = new ArrayList<String>();
-        for (WebElementFacade item : itemsInCart) {
-            items.add(item.findElement(By.xpath("/strong")).getText());
-        }
-        return items;
-    }
+
+   public List<String> getItemName(){
+      List<WebElement> items = itemInCart.findElements(By.xpath("/strong"));
+      List<String> itemTitle = new ArrayList<String>();
+      for(WebElement item : items){
+         itemTitle.add(item.getText());
+      }
+      return  itemTitle;
+   }
 }
--- src/test/java/com/serenitybdd/jbehave/AddItemToShoppingCartStepDefinitions.java
+++ src/test/java/com/serenitybdd/jbehave/AddItemToShoppingCartStepDefinitions.java
@@ -1,14 +1,15 @@
 package com.serenitybdd.jbehave;
 
 import com.serenitybdd.jbehave.steps.BuyerSteps;
-import net.serenitybdd.jbehave.SerenityStory;
 import net.thucydides.core.annotations.Steps;
 import org.jbehave.core.annotations.Given;
 import org.jbehave.core.annotations.Then;
 import org.jbehave.core.annotations.When;
+import static org.assertj.core.api.Assertions.*;
 
 
-public class ShoppingCartStepDefinitions {
+public class AddItemToShoppingCartStepDefinitions {
+
     @Steps
     BuyerSteps buyer;
 
@@ -26,10 +27,11 @@ public class ShoppingCartStepDefinitions {
         buyer.click_on_add_da_vinci_to_shopping_cart();
     }
 
-    @Then("Chosen items should appear in cart")
-    public void shouldSeeLoginForm(){
+    @Then("Chosen $firstBook and $secondBook should appear in cart")
+    public void shouldSeeLoginForm(String firstBook, String secondBook){
         buyer.click_on_shopping_cart();
-        buyer.check_items_in_shopping_cart();
+        assertThat(buyer.check_items_in_shopping_cart().contains(firstBook));
+        assertThat(buyer.check_items_in_shopping_cart().contains(secondBook));
 
     }
 
